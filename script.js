33a77f6e6c57e10870463b60b4487f2cdd062065
wendynavarrolima-glitch <wendy.navarro.lima@escola.pr.gov.br>
2026-09-24 12:28:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+function entrar(){
+
+let nome=document.getElementById("nome").value;
+
+if(nome==""){
+
+alert("Digite seu nome.");
+
+return;
+
+}
+
+localStorage.setItem("usuario",nome);
+
+window.location="usuario.html";
+
+}
+
+function sair(){
+
+localStorage.removeItem("usuario");
+
+window.location="index.html";
+
+}
+
+if(window.location.pathname.includes("usuario.html")){
+
+let nome=localStorage.getItem("usuario");
+
+document.getElementById("mensagem").innerHTML=
+
+"Bem-vindo, "+nome+"!";
+
+}
+
+if('serviceWorker' in navigator){
+
+navigator.serviceWorker.register('sw.js');
+
+}
 
